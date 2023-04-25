f2b7ad0749acf89f04357a443eaa45373a588b24
HarryHoliday <12356741@daum.net>
2023-04-26 01:13:57 +0900

    dev

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'hw^_^dev',
+  title: 'hwondev',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -83,7 +83,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'hw^_^dev',
+        title: 'hwondev',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -105,7 +105,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} hwondev, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
