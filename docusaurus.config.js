f7b01ef5718abc0296ae769f80c7e4e21a11185b
HarryHoliday <12356741@daum.net>
2023-05-07 22:10:40 +0900

    dev

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          // { to: '/blog', label: 'Blog', position: 'left' },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/HarryHoliday/HarryHoliday.github.io",
             label: "GitHub",
